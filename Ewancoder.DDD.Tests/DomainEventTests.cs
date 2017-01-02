55955d7748f57105e2bda1940a3b3ea46d6186e3
ewancoder <ewancoder@gmail.com>
2017-01-03 00:25:05 +0300

    Test DomainEvent

---
+++ Ewancoder.DDD.Tests/DomainEventTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Ewancoder.DDD.Tests
+{
+    using System;
+    using Xunit;
+
+    public sealed class DomainEventTests
+    {
+        [Fact]
+        public void ShouldSetStreamId()
+        {
+            var streamId = Guid.NewGuid();
+
+            var sut = new TestDomainEvent(streamId);
+
+            Assert.Equal(streamId, sut.StreamId);
+        }
+    }
+}
---
+++ Ewancoder.DDD.Tests/TestDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Ewancoder.DDD.Tests
+{
+    using System;
+
+    public sealed class TestDomainEvent : DomainEvent
+    {
+        public TestDomainEvent(Guid streamId) : base(streamId)
+        {
+        }
+    }
+}
