042f7c5f34d742ea7bed6924268d1d3ac9d1f6e1
ewancoder <ewancoder@gmail.com>
2017-01-05 20:30:34 +0300

    Test EventStream

---
+++ Ewancoder.DDD.Tests/EventStreamTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Ewancoder.DDD.Tests
+{
+    using System;
+    using Xunit;
+    using Exceptions;
+
+    public sealed class EventStreamTests
+    {
+        private readonly TestEventStream _sut;
+
+        public EventStreamTests()
+        {
+            _sut = new TestEventStream();
+        }
+
+        [Fact]
+        public void ShouldFailIfApplierNotRegistered()
+        {
+            Assert.Throws<NoEventApplierRegisteredException>(
+                () => _sut.TestApplyChange(new TestEvent(Guid.Empty)));
+        }
+
+        [Fact]
+        public void ShouldApplyRegisteredEvent()
+        {
+            TestEvent result = null;
+            _sut.TestRegisterApplier<TestEvent>(e => result = e);
+
+            var testEvent = new TestEvent(Guid.Empty);
+            _sut.TestApplyChange(testEvent);
+            Assert.Equal(testEvent, result);
+        }
+
+        [Fact]
+        public void ShouldHaveDefaultVersionNegativeOne()
+        {
+            Assert.Equal(-1, _sut.TestVersion);
+        }
+
+        [Fact]
+        public void ShouldNotIncrementVersionAfterEventApplied()
+        {
+            // Should increment it only after CommitChanges() is called.
+            _sut.TestRegisterApplier<TestEvent>(e => { });
+            _sut.TestApplyChange(new TestEvent(Guid.Empty));
+            Assert.Equal(-1, _sut.TestVersion);
+        }
+
+        [Fact]
+        public void ShouldRegisterAppliersAtConstruction()
+        {
+            Assert.True(_sut.RegisteredAppliers);
+        }
+
+        [Fact]
+        public void ShouldRegisterAppliersAtStatefulConstruction()
+        {
+            Assert.True(new TestEventStream(new TestSnapshot()).RegisteredAppliers);
+        }
+
+        [Fact]
+        public void ShouldSetVersionAndIdFromState()
+        {
+            var id = Guid.NewGuid();
+            var version = 33;
+            var snapshot = new TestSnapshot
+            {
+                Id = id,
+                Version = version
+            };
+            var sut = new TestEventStream(snapshot);
+
+            Assert.Equal(id, sut.TestId);
+            Assert.Equal(version, sut.TestVersion);
+        }
+    }
+}
--- Ewancoder.DDD.Tests/TestEventStream.cs
+++ Ewancoder.DDD.Tests/TestEventStream.cs
@@ -1,9 +1,31 @@
 ﻿namespace Ewancoder.DDD.Tests
 {
+    using System;
+    using Interfaces;
+
     public sealed class TestEventStream : EventStream
     {
+        public TestEventStream() { }
+        public TestEventStream(IEventStreamSnapshot snapshot) : base(snapshot) { }
+
+        public bool RegisteredAppliers { get; private set; }
+        public Guid TestId => Id;
+        public int TestVersion => Version;
+
+        public void TestRegisterApplier<TDomainEvent>(Action<TDomainEvent> applier)
+            where TDomainEvent : IDomainEvent
+        {
+            base.RegisterApplier(applier);
+        }
+
+        public void TestApplyChange(IDomainEvent @event)
+        {
+            base.ApplyChange(@event);
+        }
+
         protected override void RegisterAppliers()
         {
+            RegisteredAppliers = true;
         }
     }
 }
