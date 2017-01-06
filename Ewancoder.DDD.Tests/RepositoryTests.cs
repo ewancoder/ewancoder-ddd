74c61528ad72a708e1167466ad2e882e4394d87c
ewancoder <ewancoder@gmail.com>
2017-01-06 19:14:12 +0300

    Minimal snapshot repository tests

--- Ewancoder.DDD.Tests/RepositoryTests.cs
+++ Ewancoder.DDD.Tests/RepositoryTests.cs
@@ -1,20 +1,21 @@
 ﻿namespace Ewancoder.DDD.Tests
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading;
     using Moq;
     using Xunit;
     using Exceptions;
     using Interfaces;
-    using System.Threading.Tasks;
-    using System.Threading;
-    using System.Collections.Generic;
-    using System.Linq;
 
     public sealed class RepositoryTests
     {
         private readonly Mock<IEventDispatcher> _dispatcher;
         private readonly Mock<IEventStore> _eventStore;
         private readonly Repository<TestEventStream> _sut;
+        private readonly Repository<TestEventStream, TestSnapshot> _sutWithSnapshots;
+        private readonly Mock<ISnapshotFactory<TestEventStream, TestSnapshot>> _snapshotFactory;
+        private readonly Mock<ISnapshotStore<TestSnapshot>> _snapshotStore;
 
         public RepositoryTests()
         {
@@ -24,6 +25,17 @@
                 _dispatcher.Object,
                 _eventStore.Object,
                 2);
+
+            _snapshotFactory = new Mock<ISnapshotFactory<TestEventStream, TestSnapshot>>();
+            _snapshotStore = new Mock<ISnapshotStore<TestSnapshot>>();
+
+            _sutWithSnapshots = new Repository<TestEventStream, TestSnapshot>(
+                _dispatcher.Object,
+                _eventStore.Object,
+                2,
+                _snapshotFactory.Object,
+                _snapshotStore.Object,
+                3);
         }
 
         [Fact]
@@ -36,10 +48,26 @@
         }
 
         [Fact]
+        public void ShouldThrowIfStreamNotExistsWithSnapshots()
+        {
+            var id = Guid.NewGuid();
+            _eventStore.Setup(s => s.GetLastStreamVersion(id)).Returns(-1);
+
+            Assert.Throws<EventStreamDoesNotExistException>(() => _sutWithSnapshots.GetById(id));
+        }
+
+        [Fact]
         public void ShouldThrowIfNoEventsForEventStreamLoaded()
         {
             Assert.Throws<UnableToGetEventsForStreamException>(
                 () => _sut.GetById(Guid.NewGuid()));
+        }
+
+        [Fact]
+        public void ShouldThrowIfNoEventsForEventStreamLoadedWithSnapshots()
+        {
+            Assert.Throws<UnableToGetEventsForStreamException>(
+                () => _sutWithSnapshots.GetById(Guid.NewGuid()));
         }
 
         [Fact]
@@ -65,8 +93,33 @@
                     new TestAppliedEvent("prop5")
                 });
 
-            var stream = _sut.GetById(id);
-            Assert.Equal("prop5", stream.Prop);
+            Assert.Equal("prop5", _sut.GetById(id).Prop);
+        }
+
+        [Fact]
+        public void ShouldGetByIdWithSnapshots()
+        {
+            var id = Guid.NewGuid();
+            _eventStore.Setup(s => s.GetLastStreamVersion(id)).Returns(4);
+            _eventStore.Setup(s => s.GetEventsForStream(id, 0, 2))
+                .Returns(new IDomainEvent[]
+                {
+                    new TestAppliedEvent("prop1"),
+                    new TestAppliedEvent("prop2")
+                });
+            _eventStore.Setup(s => s.GetEventsForStream(id, 2, 2))
+                .Returns(new IDomainEvent[]
+                {
+                    new TestAppliedEvent("prop3"),
+                    new TestAppliedEvent("prop4")
+                });
+            _eventStore.Setup(s => s.GetEventsForStream(id, 4, 2))
+                .Returns(new IDomainEvent[]
+                {
+                    new TestAppliedEvent("prop5")
+                });
+
+            Assert.Equal("prop5", _sutWithSnapshots.GetById(id).Prop);
         }
 
         [Fact]
@@ -86,6 +139,32 @@
             _eventStore.Setup(s => s.Save(Guid.Empty, It.IsAny<IEnumerable<IDomainEvent>>(), -1))
                 .Callback(() => saved = true);
             _sut.Save(stream);
+            Assert.True(saved);
+            Assert.Equal(2, stream.TestVersion);
+
+            Thread.Sleep(1000);
+            _dispatcher.Verify(d => d.Dispatch(e1));
+            _dispatcher.Verify(d => d.Dispatch(e2));
+            _dispatcher.Verify(d => d.Dispatch(e3));
+        }
+
+        [Fact]
+        public void ShouldSaveWithSnapshots()
+        {
+            var stream = new TestEventStream();
+            var e1 = new TestAppliedEvent("prop1");
+            var e2 = new TestAppliedEvent("prop2");
+            var e3 = new TestAppliedEvent("prop3");
+
+            stream.TestApplyChange(e1);
+            stream.TestApplyChange(e2);
+            stream.TestApplyChange(e3);
+            Assert.Equal("prop3", stream.Prop);
+
+            var saved = false;
+            _eventStore.Setup(s => s.Save(Guid.Empty, It.IsAny<IEnumerable<IDomainEvent>>(), -1))
+                .Callback(() => saved = true);
+            _sutWithSnapshots.Save(stream);
             Assert.True(saved);
             Assert.Equal(2, stream.TestVersion);
 
