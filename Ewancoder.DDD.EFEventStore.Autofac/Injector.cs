2749163ab4587927171f47f47332b5341e5026e4
ewancoder <ewancoder@gmail.com>
2017-01-14 15:57:34 +0300

    EFEventStore autofac configuration

---
+++ Ewancoder.DDD.EFEventStore.Autofac/Injector.cs
@@ -0,0 +1,36 @@
+﻿namespace Ewancoder.DDD.EFEventStore.Autofac
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Autofac;
+    using Interfaces;
+
+    /// <summary>
+    /// Injects all dependencies into autofac container.
+    /// </summary>
+    public static class Injector
+    {
+        /// <summary>
+        /// Registers all dependencies in container builder.
+        /// </summary>
+        /// <param name="builder">Autofac container builder.</param>
+        /// <returns>Autofac container builder with registered dependencies.</returns>
+        public static ContainerBuilder Setup(
+            ContainerBuilder builder,
+            IDictionary<string, Type> knownEventTypes,
+            IDictionary<string, Type> knownSnapshotTypes)
+        {
+            builder.RegisterType<EventStore>()
+                .As<IEventStore>()
+                .WithParameter("serializer", new Serializer(
+                    new TypeFactory(knownEventTypes)));
+
+            builder.RegisterGeneric(typeof(SnapshotStore<>))
+                .As(typeof(ISnapshotStore<>))
+                .WithParameter("serializer", new Serializer(
+                    new TypeFactory(knownSnapshotTypes)));
+
+            return builder;
+        }
+    }
+}
---
+++ Ewancoder.DDD.EFEventStore.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ewancoder.DDD.EFEventStore.Autofac")]
+[assembly: AssemblyDescription("Ewancoder DDD EFEventStore Autofac")]
+[assembly: AssemblyCompany("Ewancoder")]
+[assembly: AssemblyProduct("Ewancoder.DDD.EFEventStore.Autofac")]
+[assembly: AssemblyCopyright("Copyright © Ewancoder 2017")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("26c60810-711f-4983-8339-9b27cf75b3de")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("v1.0")]
