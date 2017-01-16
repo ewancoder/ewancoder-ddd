a8efe70f5155ea1ba39a88bb387b096bc1da6fb9
ewancoder <ewancoder@gmail.com>
2017-01-16 19:22:13 +0300

    Add SyncOrderedEventDispatcher for apps without eventual consistency

---
+++ Ewancoder.DDD.Tests/SyncOrderedEventDispatcherTests - Copy.cs
@@ -0,0 +1,29 @@
+﻿namespace Ewancoder.DDD.Tests
+{
+    using System;
+    using System.Threading;
+    using Moq;
+    using Xunit;
+    using Interfaces;
+
+    public sealed class SyncOrderedEventDispatcherTests
+    {
+        [Fact]
+        public void ShouldDispatchEventsInOrder()
+        {
+            var e1 = new TestEvent(Guid.Empty);
+            var e2 = new TestEvent(Guid.Empty);
+
+            var dispatcher = new Mock<IEventDispatcher>();
+            var sut = new SyncOrderedEventDispatcher(dispatcher.Object);
+
+            sut.Dispatch(new[]
+            {
+                e1, e2
+            });
+
+            dispatcher.Verify(d => d.Dispatch(e1));
+            dispatcher.Verify(d => d.Dispatch(e2));
+        }
+    }
+}
---
+++ Ewancoder.DDD/SyncOrderedEventDispatcher.cs
@@ -0,0 +1,38 @@
+﻿namespace Ewancoder.DDD
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using Interfaces;
+
+    /// <summary>
+    /// Synchronous ordered event dispatcher. Use it to build applications without
+    /// eventual consistency.
+    /// </summary>
+    public sealed class SyncOrderedEventDispatcher : IOrderedEventDispatcher
+    {
+        /// <summary>
+        /// Used to dispatch events one by one.
+        /// </summary>
+        private readonly IEventDispatcher _dispatcher;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="SyncOrderedEventDispatcher"/> class.
+        /// </summary>
+        /// <param name="dispatcher">Event dispatcher.</param>
+        public SyncOrderedEventDispatcher(IEventDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+        /// <summary>
+        /// Dispatches ordered events synchronously.
+        /// </summary>
+        /// <param name="events">Ordered events.</param>
+        public void Dispatch(IEnumerable<IDomainEvent> events)
+        {
+            foreach (var @event in events)
+                _dispatcher.Dispatch((dynamic)@event);
+        }
+    }
+}
