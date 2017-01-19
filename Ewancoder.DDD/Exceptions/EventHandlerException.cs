18593bd9ecb2c3dba14d3bcf44bb28513ccae2eb
ewancoder <ewancoder@gmail.com>
2017-01-19 23:10:58 +0300

    Add ThrowExceptionInterceptor that throws on exception

---
+++ Ewancoder.DDD/Exceptions/EventHandlerException.cs
@@ -0,0 +1,42 @@
+﻿namespace Ewancoder.DDD.Interfaces
+{
+    using System;
+
+    /// <summary>
+    /// Thrown when using ThrowExceptionInterceptor and event handler throws an
+    /// exception.
+    /// </summary>
+    public sealed class EventHandlerException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="EventHandlerException"/> class.
+        /// </summary>
+        /// <param name="event">Domain event.</param>
+        /// <param name="eventHandler">Event handler.</param>
+        /// <param name="exception">Exception.</param>
+        public EventHandlerException(
+            object @event, object eventHandler, Exception exception)
+            : base(exception.Message, exception)
+        {
+            Event = @event;
+            EventHandler = eventHandler;
+            Exception = exception;
+        }
+
+        /// <summary>
+        /// Gets domain event that caused the exception.
+        /// </summary>
+        public object Event { get; }
+
+        /// <summary>
+        /// Gets event handler that thrown the exception.
+        /// </summary>
+        public object EventHandler { get; }
+
+        /// <summary>
+        /// Gets the exception that was thrown.
+        /// </summary>
+        public Exception Exception { get; }
+    }
+}
---
+++ Ewancoder.DDD/Interfaces/ThrowExceptionInterceptor.cs
@@ -0,0 +1,32 @@
+﻿namespace Ewancoder.DDD.Interfaces
+{
+    using System;
+
+    /// <summary>
+    /// Event handler exception interceptor that throws exception through.
+    /// </summary>
+    public sealed class ThrowExceptionInterceptor
+        : IEventHandlerExceptionInterceptor
+    {
+        /// <summary>
+        /// Intercepts exception thrown by handling a domain event. Throws
+        /// exception through.
+        /// </summary>
+        /// <typeparam name="TDomainEvent">Domain event type that has been
+        /// handled with exception.</typeparam>
+        /// <typeparam name="TEventHandler">Event handler type that caused the exception.</typeparam>
+        /// <param name="event">Domain event that has been handled with exception.</param>
+        /// <param name="eventHandler">Event handler that caused the exception.</param>
+        /// <param name="exception">Exception that has been thrown by the
+        /// event handler while handling given event.</param>
+        public void Intercept<TDomainEvent, TEventHandler>(
+            TDomainEvent @event,
+            TEventHandler eventHandler,
+            Exception exception)
+            where TDomainEvent : IDomainEvent
+            where TEventHandler : IEventHandler<TDomainEvent>
+        {
+            throw new EventHandlerException(@event, eventHandler, exception);
+        }
+    }
+}
